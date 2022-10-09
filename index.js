91b50b2054556813414f6e79e199c16a7f6080f9
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2022-10-09 09:33:19 -0600

    Added skeleton of index.js

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
+const inquirer = require('inquirer')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 
+let employees = []
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: `What is the Manager's name?`,
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: `What is the Manager's id?`,
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: `What is the Manager's email?`,
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: `What is the Manager's office number?`,
+            name: 'officeNumber'
+        }
+    ]).then((data) =>{
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+        employees.push(manager)
+    })
+    function menu(){
+        inquirer    
+            .prompt([
+                {
+                    type: 'list',
+                    message: 'Which type of employee would you like to add to the team?',
+                    choices: ['Intern', 'Engineer', 'End & Build Team']
+                },
+
+            ]).then((data) => {
+                if(data.choices === 'Intern'){
+
+                }else if(data.choices === 'Engineer'){
+
+                }else{
+                    
+                }
+            })
+    }
